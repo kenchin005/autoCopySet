db54f8685791c3735a525d60e2ca1d8a2be6624b
kenchin005 <60567898+kenchin005@users.noreply.github.com>
2021-02-19 23:54:29 +0900

    219

---
+++ shift.js
@@ -0,0 +1,19 @@
+function shiftset() {
+    var spreadsheet = SpreadsheetApp.openById("1eXLp8JhAJKDKAq01yTOgh_61GbtllMnhihSIpmMt3-s");
+    var sheet = spreadsheet.getSheetByName("3月(光通信)  ");
+    
+    var month = new Date().getMonth()+1;
+    var year = new Date().getFullYear();
+    var lastday = new Date(year,month,0);
+
+    console.log(year);
+    console.log(month);
+    console.log(lastday);
+
+    for(var i = 1;i<lastday;i++){
+
+    }
+        
+    
+    
+}
