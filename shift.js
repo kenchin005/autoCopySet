4c1ae7db70df44f83503cfa83946737457e91344
kenchin005 <60567898+kenchin005@users.noreply.github.com>
2021-02-20 00:19:04 +0900

    0220

--- shift.js
+++ shift.js
@@ -1,23 +1,24 @@
 function shiftset() {
     var spreadsheet = SpreadsheetApp.openById("1eXLp8JhAJKDKAq01yTOgh_61GbtllMnhihSIpmMt3-s");
-    var sheet = spreadsheet.getSheetByName("3月(光通信)  ");
+    var sheet = spreadsheet.getSheetByName("2月(光通信) ");
     
     var month = new Date().getMonth()+1;
     var year = new Date().getFullYear();
-    var lastday = new Date(year,month,0);
+    var lastday = new Date(year,month,0).getDate();
 
     console.log(year);
     console.log(month);
     console.log(lastday);
 
 
-    var values = sheet.getRange(4,5,1,30).getValues();
+    var values = sheet.getRange(23,5,1,lastday).getValues();
 
     console.log(values);
 
-    // for(var i = 1;i<lastday;i++){
-
-    // }
+   for(var i = 0;i<lastday;i++){
+     console.log(values[0][i]);
+     
+  }
         
     
     
