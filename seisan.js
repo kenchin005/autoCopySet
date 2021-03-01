4fc01faff02d8d53fecf798095468c4f974494ed
kenchin005 <60567898+kenchin005@users.noreply.github.com>
2021-03-02 00:50:45 +0900

    0302

---
+++ seisan.js
@@ -0,0 +1,24 @@
+function myFunction() {
+    　var spread = SpreadsheetApp.getActiveSheet();
+    
+    for(var i =1;i<32;i++){
+    
+        if(){
+            値に掛け算ex2
+          もし値が入っていたらdate[i]に.push;
+        }
+    
+      var date = {};
+       date[i] = spread.getRange(2,i+2,39,1).getValues();
+       console.log(date[i]);
+    
+       var total = date.reduce((sum, element) => sum + element, 0);
+    
+    }
+    
+    
+    
+      // var mnp = spread.getRange(2,3);
+    
+    
+    }
