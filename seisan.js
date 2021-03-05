582769c21e964ae72ca64626cbb4b31270f44dd9
kenchin005 <60567898+kenchin005@users.noreply.github.com>
2021-03-05 15:13:51 +0900

    0305

--- seisan.js
+++ seisan.js
@@ -1,16 +1,103 @@
 function myFunction() {
     　var spread = SpreadsheetApp.getActiveSheet();
+    var mnp = 2;
+    var junsin = 1;
+    var ymsb = 1;
+    var simmnp = 1.6;
+    var simjunsin = 0.8;
+    var simymsb = 0.65;
+    var jurai = 0.5;
+    var tab = 0.5;
+    var onlyData = 0.5;
+    var other = 0.1;
+    var sKihen = 0.8;
+
+
+//YM
+  var yMnp = 1;
+  var yJunsin = 0.5;
+  var ySbym = 0.25;
+  var ysimMnp = 0.8;
+  var ysimjunsin = 0.4;
+  var ysimSbym = 0.2;
+  var yjurai = 0.4;
+  var yTab = 0.5;
+  var yDataonly = 0.5;
+  var yOther = 0.1;
+  var yKihen = 0.4;
+
+  //固定
+  var sbHikari = 2.25;
+  var air = 1.5;
+  var airRental = 1;
+  var airKihen = 1.5;
+
+
+
+  //その他
+  var denki = 0.5;
+  var tel = 0.5;
+  var cable = 1;
+  var paypay = 0.1;
+  var mysb = 0.1;
+  //特販
+  var tokuhan = 2; 
+  
+  var date = {};
+  date[i] = spread.getRange(2,i+2,39,1).getValues();
+  console.log(date[i]);
+  
     
+
+
     for(var i =1;i<32;i++){
+
+      for(var j = 1;j<=36;j++){
+        switch (j) {
+          case 1:
+          date[j][i]*mnp;  
+            break;
+        
+          case 2:
+
+            break;
+
+          case 3:
+
+            break;  
+          case 4:
+
+            break;
+          
+          case 4:
+
+            break;
+          
+          case 4:
+
+            break;
+          case 4:
+
+            break;
+          case 4:
+
+            break;
+          
+
+
+          default:
+            break;
+        }
+
+
+      }
+      
     
         if(){
-            値に掛け算ex2
+            
           もし値が入っていたらdate[i]に.push;
-        }
     
-      var date = {};
-       date[i] = spread.getRange(2,i+2,39,1).getValues();
-       console.log(date[i]);
+      
     
        var total = date.reduce((sum, element) => sum + element, 0);
     
